e2d009135076a16cde5a0a92e48877ecdb16ed35
iJullaa <sj53927@zut.edu.pl>
2025-09-05 00:24:12 +0200

    Now everything works well

--- src/components/VideoModal.js
+++ src/components/VideoModal.js
@@ -1,22 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './VideoModal.css';
 
 import myDroneVideo from '../assets/dron_video.mp4';
 
 const VideoModal = ({ isOpen, onClose }) => {
-  if (!isOpen) {
-    return null;
-  }
-
-  const videoRef = React.useRef(null);
+  const videoRef = useRef(null);
 
   useEffect(() => {
     if (isOpen && videoRef.current) {
       videoRef.current.play().catch((error) => {
-        console.warn('Autoplay was prevented:', error);
+        console.warn(
+          'Autoplay was prevented. This is normal if video has sound and is not muted.',
+          error
+        );
       });
     }
   }, [isOpen]);
+
+  if (!isOpen) {
+    return null;
+  }
 
   return (
     <div className="modal-overlay" onClick={onClose}>
